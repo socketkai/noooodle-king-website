ebba110911e849f85db6a9e1533ecb82a9f7dccd
Kaixiang Zhang <socketkai@gmail.com>
2020-04-22 23:06:28 +0800

    blog

--- nkwebsite/__init__.py
+++ nkwebsite/__init__.py
@@ -36,9 +36,10 @@ def create_app(test_config=None):
     db.init_app(app)
 
     # apply the blueprints to the app
-    from nkwebsite import auth
+    from nkwebsite import auth, blog
 
     app.register_blueprint(auth.bp)
+    app.register_blueprint(blog.bp)
 
     # make url_for('index') == url_for('blog.index')
     # in another app, you might define a separate main index here with
